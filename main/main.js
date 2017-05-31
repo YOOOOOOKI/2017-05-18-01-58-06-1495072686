0d6c412b619fe1d209675363df48ea4271f456d9
yuki <172404460@qq.com>
2017-05-31 09:46:57 +0800

    sum
    增加了算字符串总数的部分

--- main/main.js
+++ main/main.js
@@ -1,3 +1,6 @@
-module.exports = function main() {
+module.exports = function main(a) {
   // Write your cade here
+  var b=parseInt(a);
+  var sum=parseInt(b/100)+parseInt(b%100/10)+b%100%10;
+  return sum;
 };
